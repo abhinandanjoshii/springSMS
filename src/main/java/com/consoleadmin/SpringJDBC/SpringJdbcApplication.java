a83b5e5b67d14e0ed77228f875e00ed068859cb5
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2024-12-23 14:58:31 +0530

    inital check without jpa

--- src/main/java/com/consoleadmin/SpringJDBC/SpringJdbcApplication.java
+++ src/main/java/com/consoleadmin/SpringJDBC/SpringJdbcApplication.java
@@ -1,13 +1,30 @@
 package com.consoleadmin.SpringJDBC;
 
+import com.consoleadmin.SpringJDBC.model.Student;
+import com.consoleadmin.SpringJDBC.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+
+		Student s =  new Student();
+		s.setRollNo(101);
+		s.setName("Abhi");
+		s.setMarks(98);
+
+		StudentService service = context.getBean(StudentService.class);
+
+		service.addStudent(s);
+
+		List<Student> students = service.getStudents();
+		System.out.println(students);
 
 	}
 
