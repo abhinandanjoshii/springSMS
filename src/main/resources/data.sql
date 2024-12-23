a6fda0dc87a5ad2ffa50815519fdc9060670d772
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2024-12-23 17:55:19 +0530

    migrating db from h2 to postgresql

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into student (rollno, name, marks) values (101, 'Harsh',75);
-insert into student (rollno, name, marks) values (102, 'Shorya',45);
-insert into student (rollno, name, marks) values (103, 'Mayank',95);
-
