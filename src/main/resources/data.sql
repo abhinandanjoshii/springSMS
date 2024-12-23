3abda22481b899a783a74a5d6f872174f5e3f10d
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2024-12-23 17:11:07 +0530

    connected h2 db with one operation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student (rollno, name, marks) values (101, 'Harsh',75);
+insert into student (rollno, name, marks) values (102, 'Shorya',45);
+insert into student (rollno, name, marks) values (103, 'Mayank',95);
+
